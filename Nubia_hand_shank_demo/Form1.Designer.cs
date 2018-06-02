9918aee2c94d50dc7fba945c69931ec932c8fb37
wangye_ndt <y.wang@newdegreetech.com>
2018-06-02 18:28:12 +0800

    Base functions
    v0.1

---
+++ Nubia_hand_shank_demo/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Nubia_hand_shank_demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.skinSplitContainer1 = new CCWin.SkinControl.SkinSplitContainer();
+            this.skinSplitContainer2 = new CCWin.SkinControl.SkinSplitContainer();
+            this.skpbHandShank = new CCWin.SkinControl.SkinPictureBox();
+            this.skrtbLog = new CCWin.SkinControl.SkinChatRichTextBox();
+            this.skbtnStart = new CCWin.SkinControl.SkinButton();
+            ((System.ComponentModel.ISupportInitialize)(this.skinSplitContainer1)).BeginInit();
+            this.skinSplitContainer1.Panel1.SuspendLayout();
+            this.skinSplitContainer1.Panel2.SuspendLayout();
+            this.skinSplitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.skinSplitContainer2)).BeginInit();
+            this.skinSplitContainer2.Panel1.SuspendLayout();
+            this.skinSplitContainer2.Panel2.SuspendLayout();
+            this.skinSplitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.skpbHandShank)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // skinSplitContainer1
+            // 
+            this.skinSplitContainer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.skinSplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.skinSplitContainer1.Location = new System.Drawing.Point(4, 28);
+            this.skinSplitContainer1.Name = "skinSplitContainer1";
+            // 
+            // skinSplitContainer1.Panel1
+            // 
+            this.skinSplitContainer1.Panel1.Controls.Add(this.skbtnStart);
+            // 
+            // skinSplitContainer1.Panel2
+            // 
+            this.skinSplitContainer1.Panel2.Controls.Add(this.skinSplitContainer2);
+            this.skinSplitContainer1.Size = new System.Drawing.Size(792, 568);
+            this.skinSplitContainer1.SplitterDistance = 280;
+            this.skinSplitContainer1.SplitterWidth = 10;
+            this.skinSplitContainer1.TabIndex = 0;
+            // 
+            // skinSplitContainer2
+            // 
+            this.skinSplitContainer2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.skinSplitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.skinSplitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.skinSplitContainer2.Name = "skinSplitContainer2";
+            this.skinSplitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // skinSplitContainer2.Panel1
+            // 
+            this.skinSplitContainer2.Panel1.Controls.Add(this.skpbHandShank);
+            // 
+            // skinSplitContainer2.Panel2
+            // 
+            this.skinSplitContainer2.Panel2.Controls.Add(this.skrtbLog);
+            this.skinSplitContainer2.Size = new System.Drawing.Size(502, 568);
+            this.skinSplitContainer2.SplitterDistance = 346;
+            this.skinSplitContainer2.TabIndex = 0;
+            // 
+            // skpbHandShank
+            // 
+            this.skpbHandShank.BackColor = System.Drawing.Color.Transparent;
+            this.skpbHandShank.BackgroundImage = global::Nubia_hand_shank_demo.Properties.Resources.HandShank;
+            this.skpbHandShank.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.skpbHandShank.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.skpbHandShank.Location = new System.Drawing.Point(0, 0);
+            this.skpbHandShank.Name = "skpbHandShank";
+            this.skpbHandShank.Size = new System.Drawing.Size(502, 346);
+            this.skpbHandShank.TabIndex = 0;
+            this.skpbHandShank.TabStop = false;
+            // 
+            // skrtbLog
+            // 
+            this.skrtbLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.skrtbLog.Location = new System.Drawing.Point(0, 0);
+            this.skrtbLog.Name = "skrtbLog";
+            this.skrtbLog.ReadOnly = true;
+            this.skrtbLog.SelectControl = null;
+            this.skrtbLog.SelectControlIndex = 0;
+            this.skrtbLog.SelectControlPoint = new System.Drawing.Point(0, 0);
+            this.skrtbLog.Size = new System.Drawing.Size(502, 218);
+            this.skrtbLog.TabIndex = 0;
+            this.skrtbLog.Text = "";
+            // 
+            // skbtnStart
+            // 
+            this.skbtnStart.BackColor = System.Drawing.Color.Transparent;
+            this.skbtnStart.ControlState = CCWin.SkinClass.ControlState.Normal;
+            this.skbtnStart.DownBack = null;
+            this.skbtnStart.Location = new System.Drawing.Point(54, 31);
+            this.skbtnStart.MouseBack = null;
+            this.skbtnStart.Name = "skbtnStart";
+            this.skbtnStart.NormlBack = null;
+            this.skbtnStart.Size = new System.Drawing.Size(168, 73);
+            this.skbtnStart.TabIndex = 0;
+            this.skbtnStart.Text = "Activate";
+            this.skbtnStart.UseVisualStyleBackColor = false;
+            this.skbtnStart.Click += new System.EventHandler(this.skbtnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.skinSplitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NDT Hand Shank Demo, y.wang@newdegreetech.com, v0.1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.skinSplitContainer1.Panel1.ResumeLayout(false);
+            this.skinSplitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.skinSplitContainer1)).EndInit();
+            this.skinSplitContainer1.ResumeLayout(false);
+            this.skinSplitContainer2.Panel1.ResumeLayout(false);
+            this.skinSplitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.skinSplitContainer2)).EndInit();
+            this.skinSplitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.skpbHandShank)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CCWin.SkinControl.SkinSplitContainer skinSplitContainer1;
+        private CCWin.SkinControl.SkinSplitContainer skinSplitContainer2;
+        private CCWin.SkinControl.SkinPictureBox skpbHandShank;
+        private CCWin.SkinControl.SkinChatRichTextBox skrtbLog;
+        private CCWin.SkinControl.SkinButton skbtnStart;
+    }
+}
+
---
+++ Nubia_hand_shank_demo/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using CCWin;
+using Nubia_hand_shank_demo.Properties;
+
+namespace Nubia_hand_shank_demo
+{
+    public partial class Form1 : CCSkinMain
+    {
+        private Thread _thrdReadFWSignal;
+        private bool _stop_read;
+        private int[] rawdata_cur = new int[4];
+        private int[] rawdata_last = new int[4];
+        private int[] baseline_cur = new int[4];
+        private int[] baseline_last = new int[4];
+        private int _channel_num = 4;
+        private int sigma_wave = 100;
+        private int sigma_tsunami = 200;
+        private StringBuilder _sb;
+        private bool _isFirstFrame;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void skbtnStart_Click(object sender, EventArgs e)
+        {
+            if (_thrdReadFWSignal == null)
+            {
+                skbtnStart.Text = "Disable";
+                skrtbLog.Enabled = false;
+                _stop_read = false;
+                _isFirstFrame = true;
+                IICoperation.IICOpen(false);
+                IICoperation.SetIICSpeed(false);
+                _sb = new StringBuilder();
+                _thrdReadFWSignal = new Thread(read_data) { IsBackground = true };
+                _thrdReadFWSignal.Start();
+            }
+            else
+            {
+                if (_thrdReadFWSignal.IsAlive)
+                {
+                    _stop_read = true;
+                    _thrdReadFWSignal.Abort();
+                    _thrdReadFWSignal = null;
+                    IICoperation.IICClose(true);
+                    skbtnStart.Text = "Activate";
+                    skrtbLog.Enabled = true;
+                }
+                save_sb_to_file();
+                call_python_draw_figure();
+            }
+        }
+
+        private void read_data()
+        {
+            while (!_stop_read)
+            {
+                Thread.Sleep(1);
+                object[] data = IICoperation.IICReadData(_channel_num);
+                int[] threshold = data[0] as int[];
+                int[] rawdata = data[1] as int[];
+                int[] forcesig = data[2] as int[];
+                int[] touch_event = data[3] as int[];
+                ShowLogUtils.ShowLog(skrtbLog,
+                    rawdata[0] + ", " +
+                    rawdata[1] + ", " +
+                    rawdata[2] + ", " +
+                    rawdata[3] + ", " +
+                    forcesig[0] + ", " +
+                    forcesig[1] + ", " +
+                    forcesig[2] + ", " +
+                    forcesig[3] + ", "
+                    );
+                Array.Copy(rawdata, rawdata_cur, rawdata.Length);
+                calc_baseline();
+                save_rawdata_and_baseline();
+                Array.Copy(baseline_cur, baseline_last, baseline_cur.Length);
+                Array.Copy(rawdata_cur, rawdata_last, rawdata_cur.Length);
+                if (touch_event[0] == 1)
+                {
+                    Invoke(new EventHandler(delegate
+                    {
+                        skpbHandShank.BackgroundImage = Resources.HandShank2;
+                        System.Media.SystemSounds.Hand.Play();
+                    }));
+                }
+                else
+                {
+                    Invoke(new EventHandler(delegate
+                    {
+                        skpbHandShank.BackgroundImage = Resources.HandShank;
+                    }));
+                }
+            }
+        }
+
+        private void calc_baseline()
+        {
+            for (int i = 0; i < _channel_num; i++)
+            {
+                if (_isFirstFrame)
+                {
+                    baseline_cur[i] = rawdata_cur[i];
+                }
+                else
+                {
+                    baseline_cur[i] = (int) (baseline_last[i] +
+                                             (rawdata_cur[i] - rawdata_last[i]) *
+                                             Math.Exp(-Math.Pow(rawdata_cur[i] - rawdata_last[i], 2) / sigma_wave) +
+                                             (rawdata_last[i] - baseline_last[i]) *
+                                             Math.Exp(-Math.Pow(rawdata_last[i] - baseline_last[i], 2) /sigma_tsunami));
+                }
+            }
+            _isFirstFrame = false;
+        }
+
+        private void save_rawdata_and_baseline()
+        {
+            rawdata_cur.ToList().ForEach(x => _sb.Append(x + "\t"));
+            baseline_cur.ToList().ForEach(x => _sb.Append(x + "\t"));
+            _sb.Append("\r\n");
+        }
+
+        private void save_sb_to_file()
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(_sb.ToString());
+            _sb = null;
+            using (FileStream fs = new FileStream("data\\" + DateTime.Now.ToString("yyyy-M-d-HH-mm-ss") + ".txt", FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                fs.Write(buffer, 0, buffer.Length);
+                fs.Close();
+                fs.Dispose();
+            }
+        }
+
+        private void call_python_draw_figure()
+        {
+            try
+            {
+                CallPython.RunPythonScript("draw.py");
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, System.Windows.Forms.FormClosingEventArgs e)
+        {
+            _stop_read = true;
+            if (_thrdReadFWSignal != null)
+            {
+                if (_thrdReadFWSignal.IsAlive)
+                {
+                    _thrdReadFWSignal.Abort();
+                    _thrdReadFWSignal = null;
+                    IICoperation.IICClose(true);
+                }
+            }
+        }
+    }
+}
